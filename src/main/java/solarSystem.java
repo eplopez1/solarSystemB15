66ce1b1efb0248fdfce7d33851525f7444a02314
eplopez1 <ephramlopez1@gmail.com>
2023-03-29 14:08:34 -0400

    The model of the solar system

---
+++ src/main/java/solarSystem.java
@@ -0,0 +1,8 @@
+public class solarSystem {
+    public static void main(String[] args) {
+        solarSystem solar=new solarSystem();
+        solar.sun="1";
+        solar.moon="2";
+        solar.planets="2";
+    }
+}
